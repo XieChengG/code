c6c3833da208deecac072e97af17d9e62d93f913
XieChengG <xiechenggang@yeah.net>
2024-10-15 18:00:00 +0800

    python操作腾讯云COS对象存储

---
+++ python/cos-operate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+
+secret_id = 'AKIDylJaaJlwrVt1tWsynLccLo1xc190atxl'
+secret_key = 'xxxxxxxxxxxxxxxxxx'
+token = None
+region = 'ap-shanghai'
+
+config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key, Token=token)
+client = CosS3Client(config)
+
+dir_name = 'bt_backup_qingliang/database/mongodb'
+file_name = 'admin_20241014.tar.gz'
+object_name = dir_name + '/' + file_name
+
+with open('../admin_20241014.tar.gz', 'rb') as f:
+    response = client.put_object(
+        Bucket='edu-xmind-1257184986',
+        Body=f,
+        Key=object_name,
+        EnableMD5=True,
+        StorageClass='STANDARD',
+        ContentType='application/x-tar'
+    )
+print(response['ETag'])
