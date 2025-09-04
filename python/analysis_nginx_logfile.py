bf9b1eb4b67dda8ab178f61b2ef6b46750454136
XieChengG <xiechenggang@yeah.net>
2025-09-04 09:38:07 +0800

    打印输出分析统计结果

--- python/analysis_nginx_logfile.py
+++ python/analysis_nginx_logfile.py
@@ -20,24 +20,58 @@ def analysis(dict_data):
     """分析nginx日志文件"""
     upstream_status_list = []
     upstream_status_not_200 = []
+    request_time_list = []
+    request_time_greater_one_second = []
+    upstream_response_time_list = []
+    upstream_response_time_greater_one_second = []
+
     for dict_member in dict_data:
         upstream_status_list.append(dict_member.get("upstream_status"))
+        request_time_list.append(dict_member.get("request_time"))
+        upstream_response_time_list.append(dict_member.get("upstream_response_time"))
 
     for number in upstream_status_list:
         if number != 200:
             upstream_status_not_200.append(number)
 
-    if len(upstream_status_not_200) > len(dict_data) / 2:
-        return 1
-    else:
-        return 0
+    for req_time in request_time_list:
+        if req_time > 1:
+            request_time_greater_one_second.append(req_time)
+
+    for upstream_resp_time in upstream_response_time_list:
+        if upstream_resp_time > 1:
+            upstream_response_time_greater_one_second.append(upstream_resp_time)
+
+    return (
+        upstream_status_not_200,
+        request_time_greater_one_second,
+        upstream_response_time_greater_one_second,
+    )
+
+
+def print_analysis_result(*args, **kwargs):
+    print_str = """------------ANALYSE RESULT------------
+    反向代理响应码非200的个数: {0}
+    请求耗时大于1秒的个数: {1}
+    反向代理响应时间大于1秒的个数: {2}"""
+    print(print_str.format(len(args[0]), len(args[1]), len(args[2])))
 
 
 if __name__ == "__main__":
     try:
         file = "/".join([base_dir, "nginx.log"])
-        print(file)
         data = transfer_dict(file)
-        print(analysis(data))
+
+        (
+            upstream_status_not_200,
+            request_time_greater_one_second,
+            upstream_resp_time_greater_one_second,
+        ) = analysis(data)
+
+        print_analysis_result(
+            upstream_status_not_200,
+            request_time_greater_one_second,
+            upstream_resp_time_greater_one_second,
+        )
     except Exception as e:
         print(e)
