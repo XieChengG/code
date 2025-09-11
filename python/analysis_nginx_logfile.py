deb178dd0b15e74fea617fe73958f093ebb538ef
XieChengG <xiechenggang@yeah.net>
2025-09-11 17:50:44 +0800

    添加分析请求平均耗时和获取请求最大耗时

--- python/analysis_nginx_logfile.py
+++ python/analysis_nginx_logfile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from ast import arg
 import json
 import os
 import sys
@@ -50,19 +51,47 @@ def analysis(dict_data):
         if upstream_resp_time > 1:
             upstream_response_time_greater_one_second.append(upstream_resp_time)
 
+    reqeust_avg_cost_time = request_avg_time(request_time_list)
+    request_max_cost_time = get_request_max_time(request_time_list)
+
     return (
         upstream_status_not_200,
         request_time_greater_one_second,
         upstream_response_time_greater_one_second,
+        reqeust_avg_cost_time,
+        request_max_cost_time,
     )
+
+
+def request_avg_time(*args, **kwargs):
+    """计算请求平均耗时"""
+    request_time_list = args[0]
+    total_cost_time = 0
+    for cost_time in request_time_list:
+        total_cost_time += cost_time
+
+    request_avg_cost_time = total_cost_time / len(request_time_list)
+    return request_avg_cost_time
+
+
+def get_request_max_time(*args, **kwargs):
+    """获取请求最大耗时"""
+    request_time_list = args[0]
+    request_max_time = 0
+    for cost_time in request_time_list:
+        if cost_time > request_max_time:
+            request_max_time = cost_time
+    return request_max_time
 
 
 def print_analysis_result(*args, **kwargs):
     print_str = """------------ANALYSE RESULT------------
     反向代理响应码非200的个数: {0}
     请求耗时大于1秒的个数: {1}
-    反向代理响应时间大于1秒的个数: {2}"""
-    print(print_str.format(len(args[0]), len(args[1]), len(args[2])))
+    反向代理响应时间大于1秒的个数: {2}
+    请求平均耗时: {3}
+    请求最大耗时: {4}"""
+    print(print_str.format(len(args[0]), len(args[1]), len(args[2]), args[3], args[4]))
 
 
 if __name__ == "__main__":
@@ -84,12 +113,16 @@ if __name__ == "__main__":
             upstream_status_not_200,
             request_time_greater_one_second,
             upstream_resp_time_greater_one_second,
+            reqeust_avg_cost_time,
+            request_max_cost_time,
         ) = analysis(data)
 
         print_analysis_result(
             upstream_status_not_200,
             request_time_greater_one_second,
             upstream_resp_time_greater_one_second,
+            reqeust_avg_cost_time,
+            request_max_cost_time,
         )
     except Exception as e:
         print(e)
